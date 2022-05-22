a0d2b458b73e72ad1b6bf6979d5753662761bee6
euxzy <rijal.m12l@gmail.com>
2022-05-22 10:10:58 +0700

    update home pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-evenly items-center">
-      <div className="">Home</div>
-      <div className="">Home</div>
+    <div className="flex h-screen w-full items-center justify-evenly bg-secondary-dark font-lexend text-primary-light">
+      <div className="w-1/3 text-right">
+        <div className="text-9xl">Home</div>
+      </div>
+      <div className="w-1/3">
+        <div className="-mb-5">Hi! My name is</div>
+        <div className="relative -left-1 text-7xl">euxzy</div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: '#0d0d0d',
+          light: '#f1f1f1',
+        },
+        secondary: {
+          dark: '#1b1b1b',
+          light: '#999999',
+        },
+      },
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
