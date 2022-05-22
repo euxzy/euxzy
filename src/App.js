def68ef2bab96c120312f60977b20f6c42a5d537
euxzy <rijal.m12l@gmail.com>
2022-05-22 08:06:44 +0700

    add home, about, contact, navbar, footer, works pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from './components/Home';
+
 function App() {
   return (
     <div>
-      <div>Hello World!</div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex justify-evenly items-center">
+      <div className="">Home</div>
+      <div className="">Home</div>
+    </div>
+  );
+}
