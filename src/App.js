7422e20245283412ed0d70a8b2d1c1833c97f300
euxzy <rijal.m12l@gmail.com>
2022-05-22 16:19:21 +0700

    add custom cursor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -8,8 +10,26 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [customCursor, setCustomCursor] = useState('default');
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener('mousemove', mouseMove);
+  }, []);
+
+  const variants = {
+    default: { x: mousePosition.x - 32, y: mousePosition.y - 32 },
+    inverse: { x: mousePosition.x - 32, y: mousePosition.y - 32, mixBlendMode: 'difference' },
+  };
+
+  const cursorIn = () => setCustomCursor('inverse');
+  const cursorOut = () => setCustomCursor('default');
+
   return (
-    <>
+    <div onMouseEnter={cursorIn} onMouseLeave={cursorOut}>
       <Router>
         <Navbar />
 
@@ -21,7 +41,8 @@ function App() {
         </Routes>
       </Router>
       <Footer />
-    </>
+      <motion.div className="pointer-events-none fixed left-0 top-0 z-50 h-16 w-16 rounded-full bg-primary-light" variants={variants} animate={customCursor} />
+    </div>
   );
 }
 
