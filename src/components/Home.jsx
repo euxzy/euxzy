3a84787977b267bc9a2e4e1cf500f32130b2eee3
euxzy <rijal.m12l@gmail.com>
2022-05-22 17:29:14 +0700

    update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-evenly bg-secondary-dark font-lexend text-primary-light">
-      <div className="w-1/3 text-right">
-        <div className="text-9xl">Home</div>
+    <div className="relative flex h-screen w-full items-center justify-evenly overflow-hidden bg-secondary-dark font-lexend text-primary-light">
+      <div className="relative flex w-1/3 justify-end">
+        <div className="absolute mt-20 h-96 w-3/5 -rotate-12 rounded-xl bg-[#464646]" />
+        <div className="absolute mt-20 h-96 w-3/5 -rotate-3 rounded-xl bg-secondary-light" />
+        <div className="relative mt-20 h-96 w-3/5 rotate-2 rounded-xl bg-primary-light" />
       </div>
       <div className="w-1/3">
-        <div className="-mb-5">Hi! My name is</div>
-        <div className="relative -left-1 text-7xl">euxzy</div>
+        <div className="-mb-5 text-base font-normal">Hi! My name is</div>
+        <div className="relative -left-1 text-7xl font-semibold">euxzy</div>
       </div>
     </div>
   );
