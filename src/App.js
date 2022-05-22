22e581c483a1598fc6ac1de35a3134bf7a4d3202
euxzy <rijal.m12l@gmail.com>
2022-05-22 11:23:08 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
+import Works from './components/Works';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="works" element={<Works />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed z-50 flex w-full pt-40 text-xl font-bold text-primary-light">
+      <div className="flex w-1/2 items-baseline">
+        <div className="w-1/3">
+          <div className="mb-1 h-1 w-1/4 bg-primary-light" />
+          <div className="mb-1 h-1 w-full bg-primary-light" />
+          <div className="h-1 w-2/3 bg-primary-light" />
+        </div>
+        <div className="flex w-1/2 justify-evenly">
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+          <Link to="works">Works</Link>
+          <Link to="contact">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
